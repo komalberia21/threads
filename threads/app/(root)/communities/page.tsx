afd09c2eb21b56036e6b4f1d393337bdcd7a19ec
komalberia21 <komal.beria21@gmail.com>
2023-08-09 14:45:29 +0530

    Initial commit

---
+++ threads/app/(root)/communities/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>no commumitoes found</div>
+  )
+}
+
+export default page
---
+++ threads/app/components/shared/RightSidebar.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const RightSidebar = () => {
+  return (
+    <section className='custom-scrollbar rightsidebar'>
+      <div className='flex flex-1 flex-col justify-start'>
+        <h3 className='text-white'>suggested communities</h3>
+      </div>
+    
+      <div className='flex flex-1 flex-col justify-start'>
+        <h3 className='text-white'>suggested communities</h3>
+      </div>
+      </section>
+    
+    
+
+    
+  )
+}
+
+export default RightSidebar
